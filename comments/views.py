bf598de946b174c6bff092d7a98845bd89c152fe
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-11-29 22:45:52 +0000

    add comments to installed apps, make migrations and migrate

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from drf_api.permissions import IsOwnerOrReadOnly
+from drf_api_project_5.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
