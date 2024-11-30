72ed66b901f454b29626ead675548ad3c05408af
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-11-30 12:32:21 +0000

    add favourites, faovourite id and comment count

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from posts.models import Post
+from favourites.models import Favourite
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,9 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    favourite_id = serializers.SerializerMethodField()
+    favourites_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -35,16 +39,25 @@ class PostSerializer(serializers.ModelSerializer):
         """Modify the representation to add the country_name field."""
         # Get the default representation from the parent class
         representation = super().to_representation(instance)
-        
         # Add the country name to the representation
         representation['country'] = instance.country.name  # Country name instead of code
-        
         return representation
+    
+    def get_favourite_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            favourite = Favourite.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return favourite.id if favourite else None
+        return None
+
 
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'country'
+            'title', 'content', 'image', 'country',
+            'favourite_id', 'favourites_count', 'comments_count',
         ]
--- posts/views.py
+++ posts/views.py
@@ -1,92 +1,56 @@
-from django.http import Http404
-from rest_framework import status, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from drf_api_project_5.permissions import IsOwnerOrReadOnly
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from django_countries import countries
 from .models import Post
 from .serializers import PostSerializer
-from drf_api_project_5.permissions import IsOwnerOrReadOnly
 
 
-class PostList(APIView):
+
+class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
+    queryset = Post.objects.annotate(
+        favourite_count=Count('favourites', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+    ordering_fields = [
+        'favourites_count',
+        'comments_count',
+        'favourites__created_at',
+    ]
 
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(
-            posts, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def post(self, request):
-        serializer = PostSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-class PostDetail(APIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PostSerializer
-
-    def get_object(self, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-            self.check_object_permissions(self.request, post)
-            return post
-        except Post.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(
-            post, context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(
-            post, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-    
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Post.objects.all()
     
 
-class CountryList(APIView):
-
+class CountryList(generics.ListAPIView):
     """
-
     API view to list all countries
-
     """
-
     permission_classes = [permissions.AllowAny]
-
-
-
-    def get(self, request):
-
-        all_countries = [{"code": code, "name": name} for code, name in countries]
-
-        return Response(all_countries)
+   
+    def get_queryset(self):
+        # Return the list of all countries as a list of dictionaries
+        return [{"name": name} for name in countries]
+    
+    def list(self, request, *args, **kwargs):
+        # Override the `list` method to return the countries as a response
+        queryset = self.get_queryset()
+        return Response(queryset)
