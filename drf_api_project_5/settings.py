9ff2c3cb30d3ee59e779c1b9e8975d2299a3aad8
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-11-30 16:16:55 +0000

    update and install requirements.txt

--- drf_api_project_5/settings.py
+++ drf_api_project_5/settings.py
@@ -88,8 +88,6 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
-
-    'dj_rest_auth.registration',
     'corsheaders',
 
     'comments',
