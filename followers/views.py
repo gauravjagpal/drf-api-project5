4df5c09f3fa816e59b7a8465e35f85ed7f046306
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-12-02 22:43:07 +0000

    fix typo in followers.views.py file. Referencing restetrieveupdatedesetroy instead of retrievedestroy

--- followers/views.py
+++ followers/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from drf_api_project_5.permissions import IsOwnerOrReadOnly
-from followers.models import Follower
-from followers.serializers import FollowerSerializer
+from .models import Follower
+from .serializers import FollowerSerializer
 
 class FollowerList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -12,7 +12,7 @@ class FollowerList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
         
         
-class FollowerDetail(generics.RetrieveUpdateDestroyAPIView):
+class FollowerDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = FollowerSerializer
     queryset = Follower.objects.all()
