b6aabaf848cb4d1eff1425a84c4ca696e35aa70e
Gaurav Jagpal <gauravjagpal@hotmail.com>
2025-01-23 11:30:56 +0000

    change activities to a varchar

--- trips/models.py
+++ trips/models.py
@@ -3,13 +3,6 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Activity(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-    
-    
 class Trip(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
@@ -19,7 +12,7 @@ class Trip(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_profile_s10tik'
     )
-    activities = models.ManyToManyField(Activity, blank=True, related_name='trips')
+    activities = models.CharField(max_length=255, blank=True)
 
     class Meta:
         ordering = ['-created_at']
