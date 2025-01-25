44d644400a351541b1e9f9b4760c9e99483cdbba
Gaurav Jagpal <gauravjagpal@hotmail.com>
2025-01-25 14:00:48 +0000

    add profile id and profile image to trip serializer

--- trips/serializers.py
+++ trips/serializers.py
@@ -6,6 +6,8 @@ class TripSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     trips_count = serializers.ReadOnlyField()
     owner_id = serializers.ReadOnlyField(source='owner.id')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -16,5 +18,6 @@ class TripSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'trip',
             'country', 'image', 'is_owner', 'trips_count',
-            'owner_id', 'activities'
+            'owner_id', 'activities', 'profile_id', 
+            'profile_image'
         ]
